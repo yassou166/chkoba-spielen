9c827ccfcdb173eb1eaf0d8da4e31d8f65dc4393
yassou166 <yassoubouajila@gmail.com>
2021-10-14 20:40:32 +0200

    hello

--- bako.js
+++ bako.js
@@ -1,16 +1,16 @@
 //preparing
 const SUITS = ["s", "c", "h", "d"]
 const VALUES = [
-  "A",
+  "1",
   "2",
   "3",
   "4",
   "5",
   "6",
   "7",
-  "J",
-  "Q",
-  "K"
+  "9",
+  "8",
+  "10"
 ]
 
 class Deck {
@@ -21,11 +21,7 @@ class Deck {
     get numberOfCards() {
       return this.cards.length
     }
-  
-    pop() {
-      return this.cards.shift()
-    }
-  
+
     push(card) {
       this.cards.push(card)
     }
@@ -58,34 +54,65 @@ class Deck {
     get color() {
       return this.suit === "c" || this.suit === "s" ? "black" : "red"
     }
-  
-    getHTML() {
-      const cardDiv = document.createElement("div")
-      cardDiv.innerText = this.suit
-      cardDiv.classList.add("card", this.color)
-      cardDiv.dataset.value = `${this.value} ${this.suit}`
-      return cardDiv
-    }
-  }
- 
 
+  }
+
+  class kaf {
+    constructor(cards) {
+      
+    }
+
+
+  }
   const wii=new Deck()
   wii.shuffle();
 
   const places=window.document.querySelectorAll(".card");
 
-  function habet(carta,index) {
-    places[index].src="./Carta/"+carta.value+"."+carta.suit+".png";
-    return carta.value+carta.suit+".png";
+  function habet(carta) {
+    let link="./Carta/"+carta.value+"."+carta.suit+".png";
+    return link;
   }
+ 
 
-for (let i = 0; i < places.length; i++) {
-  habet(wii["cards"][i],places[i])  
+  let kaf1=new kaf();
+  let yedek1=document.querySelectorAll(".Ycard")
+  /*let kaf2=new kaf();
+  let yedek2=[Element]*3*/
+  
+//game rules
+let CardIndex=0
+let turn=0
+
+function GetValue(i) {
+  if (places[i].getAttribute("src")!=="") {
+    
+    let link=habet(wii["cards"][i]);
+    let ch=link.slice(link.lastIndexOf("/"),link.length);
+    alert(ch.slice(1,ch.indexOf(".")))
+    return ch.slice(1,ch.indexOf("."))
+  }
 }
 
-  
+function GetValueFromKaf(i) {
+  if (yedek1[i].getAttribute("src")!=="") {
+    
+    let link=habet(kaf1);
+    let ch=link.slice(link.lastIndexOf("/"),link.length);
+    alert(ch.slice(1,ch.indexOf(".")))
+    return ch.slice(1,ch.indexOf("."))
+  }
+}
 
-
-
-
+for (let i = CardIndex; i < 4; i++) {
+  places[i].src=habet(wii["cards"][i]);  
+  CardIndex=i
+} 
+CardIndex=4
+for (let i = 0; i <= 2 ; i++) {
+  kaf1[i]=wii["cards"][i+CardIndex]
+  yedek1[i].src=habet(kaf1[i]);
+  //kaf2[i]=wii["cards"][i+CardIndex+3]
+}
+ 
 
